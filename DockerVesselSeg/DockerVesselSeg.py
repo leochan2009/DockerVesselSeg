b8ccbcade7540c663956967bf18328e25acea263
Longquan Chen <leochan2009@hotmail.com>
2018-06-11 13:17:58 -0400

    EHN: first working version

--- DockerVesselSeg/DockerVesselSeg.py
+++ DockerVesselSeg/DockerVesselSeg.py
@@ -5,8 +5,10 @@ from slicer.ScriptedLoadableModule import *
 import logging
 import platform
 import subprocess
-import shutil
 import threading
+from os.path import expanduser
+import SimpleITK as sitk
+import sitkUtils
 #
 # DockerVesselSeg
 #
@@ -74,6 +76,14 @@ class DockerVesselSegWidget(ScriptedLoadableModuleWidget):
     self.progressDownload.setValue(0)
     self.progressDownload.hide()
 
+    self.dockerVolumePath = ctk.ctkPathLineEdit()
+    defaultVolumePath = os.path.join(expanduser("~"), ".dockerVolume")
+    if not os.path.exists(defaultVolumePath):
+      os.makedirs(defaultVolumePath)
+    self.dockerVolumePath.setCurrentPath(defaultVolumePath)
+    dockerForm.addRow("Docker Volume Directory:", self.dockerVolumePath)
+
+
     #
     # Parameters Area
     #
@@ -100,10 +110,10 @@ class DockerVesselSegWidget(ScriptedLoadableModuleWidget):
     parametersFormLayout.addRow("Input Volume: ", self.inputSelector)
 
     #
-    # output volume selector
+    # output vessel model selector
     #
     self.outputSelector = slicer.qMRMLNodeComboBox()
-    self.outputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+    self.outputSelector.nodeTypes = ["vtkMRMLModelNode"]
     self.outputSelector.selectNodeUponCreation = True
     self.outputSelector.addEnabled = True
     self.outputSelector.removeEnabled = True
@@ -112,7 +122,7 @@ class DockerVesselSegWidget(ScriptedLoadableModuleWidget):
     self.outputSelector.showChildNodeTypes = False
     self.outputSelector.setMRMLScene( slicer.mrmlScene )
     self.outputSelector.setToolTip( "Pick the output to the algorithm." )
-    parametersFormLayout.addRow("Output Volume: ", self.outputSelector)
+    parametersFormLayout.addRow("Output vessel model: ", self.outputSelector)
 
     #
     # Apply Button
@@ -130,11 +140,20 @@ class DockerVesselSegWidget(ScriptedLoadableModuleWidget):
     # Add vertical spacer
     self.layout.addStretch(1)
 
+    self.logic = DockerVesselSegLogic()
+
     # Refresh Apply button state
     self.onSelect()
 
   def cleanup(self):
     pass
+
+  def onReload(self,moduleName="DockerVesselSeg"):
+    """Generic reload method for any scripted module.
+    ModuleWizard will subsitute correct default moduleName.
+    """
+    self.logic.clear()
+    globals()[moduleName] = slicer.util.reloadScriptedModule(moduleName)
 
   def onSelect(self):
     self.applyButton.enabled = self.inputSelector.currentNode() and self.outputSelector.currentNode()
@@ -145,7 +164,7 @@ class DockerVesselSegWidget(ScriptedLoadableModuleWidget):
       cmd = []
       cmd.append(self.dockerPath.currentPath)
       cmd.append('pull')
-      cmd.append("li3igtlab/brain-vessel-seg" + '@' + "f5b58cc46f2868d572f214796bc663ecf8e94296dcb36c02406d64269f44130b")
+      cmd.append("li3igtlab/brain-vessel-seg" + '@' + "sha256:2095141606837f364a857ec90a19efe49f8879918881a24a4cf0e72c74a2c2d2")
       p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
       print(cmd)
       parts = dict()
@@ -178,8 +197,7 @@ class DockerVesselSegWidget(ScriptedLoadableModuleWidget):
 
 
   def onApplyButton(self):
-    logic = DockerVesselSegLogic()
-    logic.run(self.inputSelector.currentNode(), self.outputSelector.currentNode())
+    self.logic.run(self.dockerPath.currentPath, self.dockerVolumePath.currentPath, self.inputSelector.currentNode(), self.outputSelector.currentNode())
 
 #
 # DockerVesselSegLogic
@@ -194,6 +212,16 @@ class DockerVesselSegLogic(ScriptedLoadableModuleLogic):
   Uses ScriptedLoadableModuleLogic base class, available at:
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
+  DOCKERVOLUMEDIMENSION = [512.0,192.0,128.0]
+  def __init__(self):
+    self.resampledVolume = slicer.mrmlScene.CreateNodeByClass("vtkMRMLScalarVolumeNode")
+    slicer.mrmlScene.AddNode(self.resampledVolume)
+
+  def clear(self):
+    if self.resampledVolume:
+      slicer.mrmlScene.RemoveNode(self.resampledVolume.GetDisplayNode())
+      slicer.mrmlScene.RemoveNode(self.resampledVolume)
+      self.resampledVolume = None
 
   def hasImageData(self,volumeNode):
     """This is an example logic method that
@@ -208,39 +236,100 @@ class DockerVesselSegLogic(ScriptedLoadableModuleLogic):
       return False
     return True
 
-  def isValidInputOutputData(self, inputVolumeNode, outputVolumeNode):
+  def isValidInputOutputData(self, inputVolumeNode, outputModel):
     """Validates if the output is not the same as input
     """
     if not inputVolumeNode:
       logging.debug('isValidInputOutputData failed: no input volume node defined')
       return False
-    if not outputVolumeNode:
-      logging.debug('isValidInputOutputData failed: no output volume node defined')
-      return False
-    if inputVolumeNode.GetID()==outputVolumeNode.GetID():
-      logging.debug('isValidInputOutputData failed: input and output volume is the same. Create a new volume for output to avoid this error.')
+    if not outputModel:
+      logging.debug('isValidInputOutputData failed: no output vessel model node defined')
       return False
     return True
 
-  def run(self, inputVolume, outputVolume, imageThreshold, enableScreenshots=0):
+  def run(self, dockerPath, dockerVolumePath, inputVolume, outputModel):
     """
     Run the actual algorithm
     """
 
-    if not self.isValidInputOutputData(inputVolume, outputVolume):
-      slicer.util.errorDisplay('Input volume is the same as output volume. Choose a different output volume.')
+    if not self.isValidInputOutputData(inputVolume, outputModel):
+      slicer.util.errorDisplay('Input volume is the same as output vessel model. Choose a different output vessel model.')
       return False
+    slicer.app.processEvents()
+    fileList = os.listdir(dockerVolumePath)
+    for fileName in fileList:
+      os.remove(os.path.join(dockerVolumePath, fileName))
+    parameters = {}
+    parameters["InputVolume"] = inputVolume.GetID()
+    inputVolumeDim = inputVolume.GetImageData().GetDimensions()
+    inputVolumeSpacing = inputVolume.GetSpacing()
+    parameters["outputPixelSpacing"] = "%.5f,%.5f,%.5f"%(inputVolumeSpacing[0]*inputVolumeDim[0]/self.DOCKERVOLUMEDIMENSION[0], \
+                                                         inputVolumeSpacing[1]*inputVolumeDim[1]/self.DOCKERVOLUMEDIMENSION[1], \
+                                                         inputVolumeSpacing[2]*inputVolumeDim[2]/self.DOCKERVOLUMEDIMENSION[2])
+    parameters["OutputVolume"]= self.resampledVolume.GetID()
+    resampleModule = slicer.modules.resamplescalarvolume
+    slicer.cli.run(resampleModule, None, parameters, wait_for_completion=True)
+    slicer.app.processEvents()
+    img = sitk.ReadImage(sitkUtils.GetSlicerITKReadWriteAddress(self.resampledVolume.GetName()))
+    fileName = "quarterVolume.nii.gz"
+    sitk.WriteImage(img, str(os.path.join(dockerVolumePath, fileName)))
+    #-----------------------------------
 
     logging.info('Processing started')
-
-    # Compute the thresholded output volume using the Threshold Scalar Volume CLI module
-    cliParams = {'InputVolume': inputVolume.GetID(), 'OutputVolume': outputVolume.GetID(), 'ThresholdValue' : imageThreshold, 'ThresholdType' : 'Above'}
-    cliNode = slicer.cli.run(slicer.modules.thresholdscalarvolume, None, cliParams, wait_for_completion=True)
+    cmd = list()
+    cmd.append(dockerPath)
+    cmd.extend(('run', '-t', '-v'))
+    cmd.append(str(dockerVolumePath) + ':' + "/workspace/data/Case1")
+    cmd.append('li3igtlab/brain-vessel-seg@sha256:2095141606837f364a857ec90a19efe49f8879918881a24a4cf0e72c74a2c2d2')
+    cmd.extend(('python3', '/workspace/NiftyNet/net_run.py', 'inference'))
+    cmd.extend(('-a', 'brainVesselSegApp.brainVesselSegApp', '-c', '/workspace/data/vesselSeg.ini'))
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    print cmd
+    progress = 0
+    while True:
+      progress += 0.15
+      slicer.app.processEvents()
+      line = p.stdout.readline()
+      if not line:
+        print "no line"
+        break
+      print(line)
+    #outputImage = sitk.ReadImage(os.path.join(dockerVolumePath, "Case1__niftynet_out.nii.gz"))
+    #sitkUtils.PushToSlicer(outputImage, "niftyOutput", 0, True)
+    imageCollection = slicer.mrmlScene.GetNodesByClassByName("vtkMRMLScalarVolumeNode", "niftyOutput")
+    if imageCollection:
+      niftyOutputNode = imageCollection.GetItemAsObject(0)
+      if niftyOutputNode:
+        matrix = vtk.vtkMatrix4x4()
+        inputVolume.GetIJKToRASMatrix(matrix)
+        niftyOutputNode.SetIJKToRASMatrix(matrix)
+        # croppedImage = slicer.mrmlScene.GetNodeByID("vtkMRMLScalarVolumeNode1")
+        invertedImage = self.inverseVTKImage(niftyOutputNode.GetImageData())
+        slicer.mrmlScene.AddNode(invertedImage)
+        invertedImage.SetIJKToRASMatrix(matrix)
+        parameters = {}
+        parameters["InputVolume"] = invertedImage.GetID()
+        parameters["Threshold"] = 0.9
+        parameters["OutputGeometry"] = outputModel.GetID()
+        grayMaker = slicer.modules.grayscalemodelmaker
+        slicer.cli.run(grayMaker, None, parameters, wait_for_completion=True)
 
     logging.info('Processing completed')
 
     return True
 
+  def inverseVTKImage(self, inputImageData):
+    imgvtk = vtk.vtkImageData()
+    imgvtk.DeepCopy(inputImageData)
+    imgvtk.GetPointData().GetScalars().FillComponent(0, 1)
+    subtractFilter = vtk.vtkImageMathematics()
+    subtractFilter.SetInput1Data(imgvtk)
+    subtractFilter.SetInput2Data(inputImageData)
+    subtractFilter.SetOperationToSubtract()  # performed inverse operation on the
+    subtractFilter.Update()
+    invertedImageNode = slicer.mrmlScene.CreateNodeByClass("vtkMRMLScalarVolumeNode")
+    invertedImageNode.SetAndObserveImageData(subtractFilter.GetOutput())
+    return invertedImageNode
 
 class DockerVesselSegTest(ScriptedLoadableModuleTest):
   """
